dfb9476a05b09a2efdee5a722147c0ac8bbd7121
Adrian Ispas <adi.ispas95@gmail.com>
2016-09-09 16:06:40 +0300

    Finish set and Dictionary

---
+++ Sets and Dictionary/Main.py
@@ -0,0 +1,9 @@
+foods = {"milk", "milk", "apple", "beef", "apple"}
+print(foods)
+
+nature = {'tree':'green', 'sky':'blue', 'river':'blue'}
+for key, value in nature.items():
+    print("The " + key + " is " + value)
+
+
+
