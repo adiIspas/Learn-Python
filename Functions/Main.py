c555c4c9fd8d9cbbbad2029f8267768a26de867e
Adrian Ispas <adi.ispas95@gmail.com>
2016-09-09 12:42:14 +0300

    Finish functions

---
+++ Functions/Main.py
@@ -0,0 +1,11 @@
+def firstFunction():
+    print("This is my first function")
+
+def euro_to_lei(euro):
+    amount = euro * 4.5
+    return amount
+
+firstFunction()
+
+conversion = euro_to_lei(10)
+print(conversion)
