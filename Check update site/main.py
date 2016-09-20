a3ed66fe1311d5a697555d674991278cba78379f
Adrian Ispas <adi.ispas95@gmail.com>
2016-09-20 19:11:51 +0300

    Add current urls

--- Check update site/main.py
+++ Check update site/main.py
@@ -5,6 +5,11 @@ import time
 
 def check_site(url, number_urls):
     urls = 0
+    current_urls = ["pdf/2016/cazare/Planificare_sedinte_cazare_2016-2017.pdf",
+                    "pdf/2016/cazare/ANUNT_CAZARE_2016-2017.pdf",
+                    "pdf/2016/cazare/CERERE_CAZARE_2016-2017.pdf",
+                    "pdf/2016/cazare/METODOLOGIE_CAZARE_2016-2017.pdf"]
+
     source_code = requests.get(url)
     plain_text = source_code.text
 
@@ -13,8 +18,9 @@ def check_site(url, number_urls):
     for link in soup.findAll('a'):
         href = link.get('href')
 
-        if href.find('pdf/2016/cazare') is not -1:
+        if href.find('pdf/2016/cazare') is not -1 and href not in current_urls:
             urls += 1
+            print(href)
 
     if urls > number_urls:
         return 1
